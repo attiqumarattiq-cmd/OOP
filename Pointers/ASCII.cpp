2baba8aef5e9cb46ebfc3adde3f4bbbda265f49c
Umar <attiqumarattiq@gmail.com>
2026-08-30 21:42:49 +0500

    Sorting Strings Using Pointer (Logic used of ASCII Value)

---
+++ Pointers/ASCII.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "String is: {'b','a', 'c', 'd'}" << endl;
+    char string[5] = {'b', 'a', 'c', 'e', 'd'};
+    cout << static_cast<int>('A') << std::endl;
+    
+    char *ptr1 = string;
+    char fight = string[0];
+    char temp;
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = i + 1; j < 5; j++)
+        {
+            if(static_cast<int>(*(ptr1 + i)) > static_cast<int>(*(ptr1 + j)))
+            {
+                temp = *(ptr1 + i);
+                *(ptr1 + i) = *(ptr1 + j);
+                *(ptr1 + j) = temp;
+            }
+        }
+    }
+
+    cout << "After rearranging array in order(from the logic of ASCII value): ";
+    for(int i = 0; i < 5; i++)
+    {
+        cout << *(ptr1 + i) << " ";
+    }
+
+
+
+    return 0;
+}
