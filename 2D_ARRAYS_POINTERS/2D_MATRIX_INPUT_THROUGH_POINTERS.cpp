5686d680fc1b52657376092d5bdc8f17033d489f
Umar <attiqumarattiq@gmail.com>
2026-09-01 16:20:04 +0500

    2D MATRIX INPUT THROUGH POINTERS

---
+++ 2D_ARRAYS_POINTERS/2D_MATRIX_INPUT_THROUGH_POINTERS.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\n==================================" << endl;
+    int matrix[3][3];
+    int (*ptr1)[3] = matrix;
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "Enter " << " Value: ";
+            cin >> *(*(ptr1 + i) + j);
+        }
+    }
+    cout << "\n==================================" << endl;
+    cout << "The entered matrix is: " << endl;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << *(*(ptr1 + i) + j) << ", ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
